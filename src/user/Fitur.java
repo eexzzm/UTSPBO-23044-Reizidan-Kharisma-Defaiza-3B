c6e7f99b8f7ac095919884ec1f8503a039664985
eexzzm <reizidankharisma@gmail.com>
2024-10-09 20:03:52 +0700

    add interface fitur

---
+++ src/user/Fitur.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package user;
+
+
+public interface Fitur {
+
+    void buatPesanan();
+    void detilPesanan();
+    void cekKamar();
+}
